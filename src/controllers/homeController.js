d847aa131827327543be088a4c50f8ab88f65c6b
Ivan Skordev <maymunskoa@yahoo.com>
2021-09-25 17:52:17 +0300

    render about page

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -6,7 +6,11 @@ const renderHome = (req, res) => {
   const cubes = cubeService.getAll();
   res.render('index', { cubes });
 };
+const renderAbout = (req, res) => {
+  res.render('about');
+};
 
 router.get('/', renderHome);
+router.get('/about', renderAbout);
 
 module.exports = router;
--- src/models/Cube.js
+++ src/models/Cube.js
@@ -2,12 +2,20 @@ const uniqid = require('uniqid');
 class Cube {
   static #cubes = [
     {
-      id:'dafgdfdgdsadfsgf326534tgvrge5t43wrqeq',
+      id: 'dafgGgDf326534tgv',
       name: 'Classic Rubik Cube',
       description: 'This is the classic. Can you solve this cube?',
       imageUrl:
         'https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Scramble.svg/1200px-Scramble.svg.png',
       difficulty: '3',
+    },
+    {
+      id: '4z3c516biktzwf38y',
+      name: 'Magic Cube',
+      description: "This is the small brother of the classic Rubik's cube.",
+      imageUrl:
+        'https://lh3.googleusercontent.com/uCld2EUp74IDupx8rBVz-lOsmfdwobiXYrpJfQhoPn7C-1g7kPXrDkQCWGKbLC6xRqBp',
+      difficulty: '2',
     },
   ];
 

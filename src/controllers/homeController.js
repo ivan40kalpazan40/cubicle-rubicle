80412503c766c59ec62b85113e8fb9ab22ae1fe4
Ivan Skordev <maymunskoa@yahoo.com>
2021-09-27 15:15:13 +0300

    add search by text functionallity

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -10,7 +10,15 @@ const renderAbout = (req, res) => {
   res.render('about');
 };
 
+const search = (req, res) => {
+  let { search, from, to } = req.query;
+  let cubes = cubeService.search(search, from, to);
+  console.log(search);
+  res.render('index', { cubes });
+};
+
 router.get('/', renderHome);
 router.get('/about', renderAbout);
+router.get('/search', search);
 
 module.exports = router;
--- src/services/cubeService.js
+++ src/services/cubeService.js
@@ -6,6 +6,9 @@ const create = (name, description, imageUrl, difficulty) => {
   let cube = new Cube(name, description, imageUrl, difficulty);
   Cube.add(cube);
 };
+const search = (text, from, to) => {
+  return Cube.cubes.filter((x) => x.name.toLowerCase().includes(text.toLowerCase()));
+};
 
-const cubeService = { create, getAll, getOne };
+const cubeService = { create, getAll, getOne, search };
 module.exports = cubeService;

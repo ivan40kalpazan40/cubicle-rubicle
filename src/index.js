d8937e79ab4119e13c194cbab0eef8b85ea90f01
Ivan Skordev <maymunskoa@yahoo.com>
2021-10-04 11:40:49 +0300

    small fix in connection to previous commit few lines deleted in index.js

--- src/index.js
+++ src/index.js
@@ -3,14 +3,12 @@ const path = require('path');
 const initHandlebars = require('./config/handlebars');
 const routes = require('./routes');
 const config = require('./config/config.json')[process.env.NODE_ENV];
-
 const initDb = require('./config/db');
 const Cube = require('./models/Cube');
-const port = 5000;
 
 const app = express();
+
 app.use(express.urlencoded({ extended: true }));
-console.log(config);
 initHandlebars(app);
 app.use(express.static(path.resolve(__dirname, './public')));
 app.use(routes);
@@ -19,7 +17,7 @@ initDb(config.DB_CONNECTION_STRING)
     console.log('Connected to DATABASE');
     app.listen(
       config.PORT,
-      console.log.bind(console, `Server is running on port ${port}...`)
+      console.log.bind(console, `Server is running on port ${config.PORT}...`)
     );
   })
   .catch((err) => console.log(`ERR:: ${console.error(err)}`));

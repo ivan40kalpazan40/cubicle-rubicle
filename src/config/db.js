41e522b2fd16a4bac2445074ffb0105bf5c283f5
Ivan Skordev <maymunskoa@yahoo.com>
2021-10-04 11:37:29 +0300

    fix to setup scripts in config and package.json

--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,7 @@
 const mongoose = require('mongoose');
 
-const connectionString =
-  'mongodb+srv://ivan:1234@cubicle.d4453.mongodb.net/database?retryWrites=true&w=majority';
-
-const initDb = () => {
+function initDb(connectionString) {
   return mongoose.connect(connectionString);
-};
+}
 
 module.exports = initDb;
--- src/index.js
+++ src/index.js
@@ -2,31 +2,24 @@ const express = require('express');
 const path = require('path');
 const initHandlebars = require('./config/handlebars');
 const routes = require('./routes');
+const config = require('./config/config.json')[process.env.NODE_ENV];
+
 const initDb = require('./config/db');
 const Cube = require('./models/Cube');
 const port = 5000;
 
-initDb()
-  .then((res) => {
-    console.log('Connected to DATABASE');
-    // Cube.create({
-    //   name: 'Snake Cube',
-    //   description:
-    //     'This is not quite a cube, but it is in the same leage of puzzles.',
-    //   imageUrl: 'https://m.media-amazon.com/images/I/51CfisCGpLL._SL1080_.jpg',
-    //   difficulty: 7,
-    // });
-
-    
-  })
-  .catch((err) => console.log(`ERR:: ${console.error(err)}`));
 const app = express();
 app.use(express.urlencoded({ extended: true }));
-
+console.log(config);
 initHandlebars(app);
 app.use(express.static(path.resolve(__dirname, './public')));
 app.use(routes);
-app.listen(
-  port,
-  console.log.bind(console, `Server is running on port ${port}...`)
-);
+initDb(config.DB_CONNECTION_STRING)
+  .then(() => {
+    console.log('Connected to DATABASE');
+    app.listen(
+      config.PORT,
+      console.log.bind(console, `Server is running on port ${port}...`)
+    );
+  })
+  .catch((err) => console.log(`ERR:: ${console.error(err)}`));
